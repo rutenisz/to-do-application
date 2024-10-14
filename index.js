637f5aeb8581ca4d76e26d0f6c98bf323933b7b5
Ruta <ruta@Robertass-MacBook-Pro.local>
2024-10-14 17:05:52 +0300

    finished to do application

--- index.js
+++ index.js
@@ -8,5 +8,35 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = inputTaskBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputTaskBox.value = "";
+  saveData();
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") { // tagName grazina uppercase
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      console.log("clicked");
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTaskList() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTaskList();
